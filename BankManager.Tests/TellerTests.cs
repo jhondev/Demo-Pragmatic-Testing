a320a240e1efd2f7135e082759ad75f7d83b3888
Jhonattan <jma200@hotmail.com>
2017-04-03 11:08:49 -0500

    SetUp

--- BankManager.Tests/TellerTests.cs
+++ BankManager.Tests/TellerTests.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace BankManager.Tests
 {
+    [TestFixture]
     public class TellerTests
     {
+        private Teller _teller;
+        [SetUp]
+        public void SetUp()
+        {
+            _teller = new Teller();
+        }
+
+        [Test]
+        public void CheckBalance_WithNoTransactions_ReturnsZeroBalance()
+        {
+            var accountBalance = _teller.CheckBalance();
+
+            const int expectedBalance = 0;
+
+            Assert.That(accountBalance, Is.EqualTo(expectedBalance),
+                "Empty account should return cero balance");
+        }
+
+        [Test]
+        public void ProcessTransaction_FirstTransaction_ReturnsCurrentBalanceSameAsTransasction()
+        {
+            const int depositAmount = 10;
+
+            var accountBalance = _teller.ProcessTransaction(depositAmount);
+
+            Assert.That(accountBalance, Is.EqualTo(depositAmount), 
+                "First Transaction shourld return the same balance");
+        }
     }
 }
--- BankManager/Teller.cs
+++ BankManager/Teller.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankManager
 {
     public class Teller
     {
+        readonly List<int> _transactions = new List<int>();
+        public int CheckBalance()
+        {
+            return _transactions.Sum();
+        }
+
+        public int ProcessTransaction(int amount)
+        {
+            _transactions.Add(amount);
+            return CheckBalance();
+        }
     }
 }
